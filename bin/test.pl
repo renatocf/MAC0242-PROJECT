3a563e93166bdaea04b38eca4f09f9fefd20cf62
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-05 12:26:53 -0300

    Adding things to test Cortex.pm

--- bin/test.pl
+++ bin/test.pl
@@ -12,11 +12,33 @@ use strict;
 use warnings;
 
 # Packages
+use Cortex;
 use RVM::RVM;
 
 #######################################################################
 #                                TESTS                                #
 #######################################################################
 
+my $brain = new Cortex;
+my @prog = $brain->gen_program("test.txt");
+
+$/ = "\n"; my $i = 1;
+print_prog(\@prog);
+
+sub print_prog {
+    my $aref = shift; my $i = 0;
+    for my $line (@$aref) {
+        print "$i: ";
+        foreach my $token (@$line) {
+            print "$token " if defined $token; 
+        }
+        print "\n"; $i++;
+    }
+}
+
+say "\nRVM ", '-' x 20, "\n";
 my $obj = new RVM;
+
+$obj->{PROG} = \@prog;
+
 say "$obj";
