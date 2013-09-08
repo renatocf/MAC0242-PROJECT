2a1174ea66fddb0362bd48c4cb0664b9b5036e20
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2013-09-08 19:55:19 -0300

    Agora com comentarios

--- lib/RVM/f_jumps.pm
+++ lib/RVM/f_jumps.pm
@@ -13,8 +13,11 @@ sub JMP
 	my $stack = shift;
 	my $i = \$obj->{'PC'};
 	my $prog = $obj->{'PROG'};
-	$$i += $arg;
-	$$i -= $arg and return -1 unless defined($$prog[$$i]);
+	$$i += $arg;     #Acrescenta o argumento no PC
+	
+	#Devolve Falha de segmentacao, caso pule para um indice que nao existe
+	$$i -= $arg and return -1 unless defined($$prog[$$i]); 
+	
 	return $stack;	
 }
 
@@ -24,19 +27,27 @@ sub JIT
 	my $arg = shift;
 	my $stack = shift;
 	my $i = \$obj->{'PC'};
-	my $j = 0;
+	my $j; #Variavel que lembra o PC se houver erro
 	my $data = $obj->{"DATA"};
-	my $test = pop @$data;
-	my $lbl = $obj->{'LABEL'};
+	my $test = pop @$data; #Variavel que sera testada
+	my $lbl = $obj->{'LABEL'}; 
 	my $prog = $obj->{'PROG'};
-	return -1 if $stack < 1;
-	return $stack - 1 unless $test;
+	
+	return -1 if $stack < 1; #Devolve Falha de segmentacao caso a pilha esteja vazia
+	return $stack - 1 unless $test; #Não faz nada se o teste for falso
+	
+	#Caso o usuario informe a linha numericamente
+	
 	if(looks_like_number($arg)) 
 	{
 		$j = $$i;
 		$$i = $arg;
-		$$i = $j and return -1 unless defined($$prog[$$i]);
+		#Devolve falha de segmentação caso pule para uma linha inexistente 
+		$$i = $j and return -1 unless defined($$prog[$$i]);  
 	}
+	
+	#Caso o usuário informe um label
+	
 	else 
 	{
 		if(defined($$lbl{$arg}))
@@ -45,7 +56,7 @@ sub JIT
 		}
 		else
 		{
-			return -3;
+			return -3; #Se não houver o label informado ele devolve um erro
 		}
 	}
 	return $stack - 1;
@@ -57,19 +68,27 @@ sub JIF
 	my $arg = shift;
 	my $stack = shift;
 	my $i = \$obj->{'PC'};
-	my $j = 0;
+	my $j; #Variavel que lembra o PC se houver erro
 	my $data = $obj->{"DATA"};
-	my $test = pop @$data;
+	my $test = pop @$data; #Variavel que sera testada
 	my $lbl = $obj->{'LABEL'};
 	my $prog = $obj->{'PROG'};
-	return -1 if $stack < 1;
-	return $stack - 1 if $test;
+	
+	return -1 if $stack < 1; #Devolve Falha de segmentacao caso a pilha esteja vazia
+	return $stack - 1 if $test; #Não faz nada se o teste for verdadeiro
+	
+	#Caso o usuario informe a linha numericamente
+	
 	if(looks_like_number($arg)) 
 	{
 		$j = $$i;
 		$$i = $arg;
+		#Devolve falha de segmentacao caso pule para uma linha inexistente
 		$$i = $j and return -1 unless defined($$prog[$$i]);
 	}
+	
+	#Caso o usuário informe um label
+	
 	else 
 	{
 		if(defined($$lbl{$arg}))
@@ -78,7 +97,7 @@ sub JIF
 		}
 		else
 		{
-			return -3;
+			return -3;   #Se não houver o label informado ele devolve um erro
 		}
 	}
 	return $stack - 1;
