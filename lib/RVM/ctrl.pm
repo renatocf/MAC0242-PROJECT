8ec2fa83549bad2287626e6ce0420c5cc0bd8810
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-10 08:46:01 -0300

    Fixing some comments in ctrl.pm

--- lib/RVM/ctrl.pm
+++ lib/RVM/ctrl.pm
@@ -6,7 +6,7 @@ use functions; #Pacote com implementações das funções de assembly.
 # Pragmas
 use strict;
 use warnings;
-use Scalar::Util qw(looks_like_number); #funcao que avalia se um escalar e um numero, ou nao.
+use Scalar::Util qw(looks_like_number); #Funcao que avalia se um escalar e um numero, ou nao.
 
 sub ctrl
 {
@@ -16,27 +16,27 @@ sub ctrl
     my $obj = shift;
     my $prog = $obj->{'PROG'}; 
     my $i = \$obj->{'PC'};
-    my $f = 0;                                  #escalar que guarda o nome do comando
-  my $arg = 0;                              #escalar que guarda o argumento do comando
+    my $f = 0;                      #escalar que guarda o nome do comando
+    my $arg = 0;                    #escalar que guarda o argumento do comando
     my $lbl = $obj->{'LABEL'};
-    my $stack = 0;                          #controle da pilha
+    my $stack = 0;                  #controle da pilha
     
     # Preparação para o loop
     
-    $f = $$prog[$$i][0];                   #carrega comando
-    $$i = 0;                               #inicia PC
+    $f = $$prog[$$i][0];                     #carrega comando
+    $$i = 0;                                 #inicia PC
     $f = "0" unless defined($$prog[$$i][0]); #para evitar uma comparacao invalida no while 
     
     # Loop
     
     while($f ne 'END')
     {
-        $arg = $$prog[$$i][1];                                                                  #carrega argumento
+        $arg = $$prog[$$i][1];                                  #carrega argumento
         $$lbl{$$prog[$$i][2]} = $$i if defined($$prog[$$i][2]); #carrega label
-        $$i++;                                                                                          #incrementa PC
+        $$i++;                                                  #incrementa PC
         if($f ne "0")
         {
-            $stack = $obj->$f($arg, $stack);   #Chama funcao
+            $stack = $obj->$f($arg, $stack);  #Chama funcao
         }
         
         if($stack == -1)
