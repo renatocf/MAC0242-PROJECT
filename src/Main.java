60edd41b3de006c874da39999b10b94a8a71ec70
Karina Awoki <k.sueminha@hotmail.com>
2013-09-30 15:57:49 -0300

    Adding Main.java for tests (initially) and the game

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import stackable.*;
+
+class Main
+{
+  public static void main(String[] args) throws StackUnderflowException
+  {
+    Stack s = new Stack();
+    
+    for(int i = 0; i<100 ; i++)
+    {
+        s.push(new Num(i));
+    }
+    
+    for(int i = 0; i<10 ; i++)
+    {
+      System.out.println(s.pop());
+    } 
+  }
+
+}
