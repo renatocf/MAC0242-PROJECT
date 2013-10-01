8cec29c7f90f5370da3a56699f0a79e722576b55
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-09-30 21:54:24 -0300

    Adding javadoc

--- src/stackable/StackUnderflowException.java
+++ src/stackable/StackUnderflowException.java
@@ -1,7 +1,15 @@
 package stackable;
 
+/**
+ * Exception used inside Stack for indicating Stack Underflow.
+ * @author Renato Cordeiro Ferreira
+ * @see    Stack
+ */
 public final class StackUnderflowException extends Exception 
 {
+    /** 
+     * Default constructor.
+     */
     public StackUnderflowException () 
     { super("Stack Underflow\n"); }
 }
