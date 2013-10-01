7277f129affad2ac793c3bb4f0acdd6fd2f093eb
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-01 10:22:05 -0300

    Changin attribute name

--- src/robot/Command.java
+++ src/robot/Command.java
@@ -4,22 +4,22 @@ import stackable.*;
 public class Command
 {
     final private String    command;
-    final private Stackable atribute;
+    final private Stackable attribute;
     final private String    label;
 
     public Command(String cmd, Stackable atr, String lbl)
     {
         this.command = cmd;
-        this.atribute = atr;
+        this.attribute = atr;
         this.label = lbl;
     }
 
     public String toString()
     {
-        return this.label + ": " + this.command + " " + this.atribute;
+        return this.label + ": " + this.command + " " + this.attribute;
     }
 
-    public String    getCommand  () { return this.command;  }
-    public Stackable getAtribute () { return this.atribute; }
-    public String    getLabel    () { return this.label;    }
+    public String    getCommand   () { return this.command;  }
+    public Stackable getAttribute () { return this.attribute; }
+    public String    getLabel     () { return this.label;    }
 }
