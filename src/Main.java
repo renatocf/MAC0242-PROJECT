c9fbe6011186977d3c802e8fe7d3f1e93c9e201b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-01 16:23:00 -0300

    Adding main which can accept tests (the program MUST end with a null pointer)

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,35 @@
 // Libraries
+import robot.*;
 import exception.*;
 import stackable.*;
+import java.util.Vector;
+import java.util.HashMap;
 
 class Main
 {
-  public static void main(String[] args) throws StackUnderflowException
-  {
-    Stack s = new Stack();
-    
-    for(int i = 0; i < 100 ; i++)
+    public static void main(String[] args) /* throws StackUnderflowException */
     {
-        s.push(new Num(i));
+        //HashMap <String, Integer> map = new HashMap <String, Integer>();
+        //map.put("RESPOSTA", 42);
+        //map.put("PRIMO", 73);
+        //
+        //System.out.println("val: " + map.get("RESPOSTA"));
+        
+        Vector<Command> PROG = new Vector<Command>();
+        Text msg = new Text("Eeeeva");
+        
+        PROG.add(new Command("PUSH", msg,  null)); 
+        PROG.add(new Command("PRN",  null, null));
+        PROG.add(new Command("END",  null, null));
+        PROG.add(null);
+        
+        RVM Walle = new RVM(PROG);
+        try {
+            Walle.ctrl();
+        }
+        catch(Exception e)
+        {
+            System.out.println("Shit: " + e);
+        }
     }
-    
-    //! for(int i = 0; i < 110; i++)
-    for(int i = 0; i < 10 ; i++)
-    {
-      System.out.println(s.pop());
-    } 
-  }
 }
