a0c1144aa37dcfec1454bf8748142c27dc73987b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-02 20:50:01 -0300

    Refactorating function class: creating new class for tests functions (still needing to test them)

--- src/robot/function/Tests.java
+++ src/robot/function/Tests.java
@@ -1,103 +1,40 @@
 package robot.function;
 
-import java.util.Vector;
+// Libraries
 import stackable.*;
 import exception.*;
 
-public class Function
+/**
+ * Assembly functions - class Tests.
+ * Provides the funcions for making 
+ * boolean comparisons between elements
+ * inside the stack of the virtual machine.
+ * 
+ * @author Renato Cordeiro Ferreira
+ * @see Function
+ * @see RMV
+ */
+public class Tests
 {
-    private int PC = 0;
-    private Stack DATA;
-    private Vector <Stackable> RAM;
-    
-    private IO    io    = new IO    (DATA);
-    private Stk   stk   = new Stk   (DATA);
-    private Arit  arit  = new Arit  (DATA);
-    private Tests tests = new Tests (DATA);
-    
-    Function(Stack stack, Vector <Stackable> ram)
+    final private Stack DATA;
+
+    /**
+     * Class constructor. 
+     * Receives a handle to the main stack.
+     * 
+     * @param Stack Data
+     */
+    Tests(Stack DATA)
     {
-        this.DATA = stack;
-        this.RAM  = ram;
+        this.DATA = DATA;
     }
     
-    Function(Stack stack, Vector <Stackable> ram, int pc)
-    {
-        this(stack, ram); this.PC = pc;
-    }
-    
-    void call(String met, Stackable arg) 
-        throws StackUnderflowException, 
-               OutOfBoundsException,
-               WrongTypeException
-    {
-        // IO functions
-        if(met.equals("PRN" )) { io.PRN ();    }
-        
-        // Stack functions
-        else if(met.equals("POP" )) { stk.POP ();    }
-        else if(met.equals("PUSH")) { stk.PUSH(arg); }
-        else if(met.equals("DUP" )) { stk.DUP ();    }
-        
-        // Arithmetic functions
-        else if(met.equals("ADD" )) { arit.ADD ();    }
-        else if(met.equals("SUB" )) { arit.SUB ();    }
-        else if(met.equals("MUL" )) { arit.MUL ();    }
-        else if(met.equals("DIV" )) { arit.DIV ();    }
-        else if(met.equals("MOD" )) { arit.MOD ();    }
-        
-        // Memory functions
-        else if(met.equals("STO" )) { STO (arg); }
-        else if(met.equals("RCL" )) { RCL (arg); }
-              // ???? Temos que passar o endereço como int, mas ele é do tipo Stackable
-        
-        // Tests functions
-        else if(met.equals("EQ" )) { tests.EQ(); }
-        else if(met.equals("GT" )) { tests.GT(); }
-        else if(met.equals("GE" )) { tests.GE(); }
-        else if(met.equals("LT" )) { tests.LT(); }
-        else if(met.equals("LE" )) { tests.LE(); }
-        else if(met.equals("NE" )) { tests.NE(); }
-    }
-    
-    //##############################################################
-    //##                      MEMORY FUNCTIONS                   ###
-    //##############################################################
-    
-    void STO(Stackable position)
-        throws OutOfBoundsException,
-               StackUnderflowException
-    {
-        int pos;
-        if(position.looksLikeNumber())
-        {
-            Num num = (Num) position;
-            pos = (int) num.getNumber();
-        }
-        else throw new StackUnderflowException();
-        RAM.set(pos, DATA.pop());
-    }
-    
-    void RCL(Stackable position) 
-        throws OutOfBoundsException,
-               StackUnderflowException
-    {
-        int pos;
-        if(position.looksLikeNumber()) 
-        {
-            Num num = (Num) position;
-            pos = (int) num.getNumber();
-        }
-        else throw new StackUnderflowException();
-        DATA.push(RAM.get(pos));
-    }
-    
-    
-    
-    //##############################################################
-    //##                      TESTS FUNCTIONS                    ###
-    //##############################################################
-    
+    /**
+     * Assembly funcion EQ. 
+     * Makes the correspondent funcion 
+     * comparison between the two elements
+     * int the top of the main stack.
+     */
     void EQ() throws WrongTypeException,
                      StackUnderflowException
     {
@@ -234,5 +171,4 @@ public class Function
             }
         }
     }
-    
 }
