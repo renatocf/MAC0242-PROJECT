cce2bb914595799dab653a657680de4d80018c74
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-02 22:14:23 -0300

    Adding javadoc, cleaning up and fixing comments

--- src/robot/function/Arit.java
+++ src/robot/function/Arit.java
@@ -30,12 +30,24 @@ public class Arit
         this.DATA = DATA;
     }
     
-    interface Operation 
+    /** 
+     * Interface for arithmetic operations,
+     * Dummy interface with the aim of being a 
+     * model for all the arithmetic operations.
+     */
+    final private interface Operation 
     {
         double op(double a, double b);
     }
     
-    void calculate(Operation op) 
+    /**
+     * General-model funcion calculate.
+     * Takes out the two arguments in the top 
+     * of the main stack and pushes the answer
+     * of the operation made over them.
+     * @param Anonymous inner class of type operation
+     */
+    final private void calculate(Operation op) 
         throws WrongTypeException,
                StackUnderflowException
     {
@@ -51,104 +63,44 @@ public class Arit
         }
         else { throw new WrongTypeException("Num"); }
     }
-    
-    /**
-     * Assembly funcion ADD.
-     * Takes out the two arguments in the top 
-     * of the main stack and pushes the operation
-     * for that.
-     */
-    void ADD() throws WrongTypeException,
+
+    public void ADD() throws WrongTypeException,
                       StackUnderflowException
     {
         calculate(new Operation() { 
             public double op(double a, double b) { return a+b; } }
         );
-//        Stackable arg1 = DATA.pop();
-//        Stackable arg2 = DATA.pop();
-//        
-//        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-//        {
-//            Num a = (Num) arg1, b = (Num) arg2;
-//            Num sum = new Num(b.getNumber() + a.getNumber());
-//            DATA.push(sum);
-//        }
-//        else { throw new WrongTypeException("Num"); }
     }
     
-    void SUB() throws WrongTypeException,
+    public void SUB() throws WrongTypeException,
                       StackUnderflowException
     {
         calculate(new Operation() { 
             public double op(double a, double b) { return a-b; } }
         );
-        /*  */
-        /* Stackable arg1 = DATA.pop(); */
-        /* Stackable arg2 = DATA.pop(); */
-        /*  */
-        /* if(arg1.looksLikeNumber() && arg2.looksLikeNumber()) */
-        /* { */
-        /*     Num a = (Num) arg1, b = (Num) arg2; */
-        /*     Num sum = new Num(b.getNumber() - a.getNumber()); */
-        /*     DATA.push(sum); */
-        /* } */
-        /* else { throw new WrongTypeException("Num"); } */
     }
     
-    void MUL() throws WrongTypeException,
+    public void MUL() throws WrongTypeException,
                       StackUnderflowException
     {
         calculate(new Operation() { 
             public double op(double a, double b) { return a*b; } }
         );
-        /*  */
-        /* Stackable arg1 = DATA.pop(); */
-        /* Stackable arg2 = DATA.pop(); */
-        /*  */
-        /* if(arg1.looksLikeNumber() && arg2.looksLikeNumber()) */
-        /* { */
-        /*     Num a = (Num) arg1, b = (Num) arg2; */
-        /*     Num sum = new Num(b.getNumber() * a.getNumber()); */
-        /*     DATA.push(sum); */
-        /* } */
-        /* else { throw new WrongTypeException("Num"); } */
     }
     
-    void DIV() throws WrongTypeException,
+    public void DIV() throws WrongTypeException,
                       StackUnderflowException
     {
         calculate(new Operation() { 
             public double op(double a, double b) { return a/b; } }
         );
-//        
-//        Stackable arg1 = DATA.pop();
-//        Stackable arg2 = DATA.pop();
-//        
-//        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-//        {
-//            Num a = (Num) arg1, b = (Num) arg2;
-//            Num sum = new Num(b.getNumber() / a.getNumber());
-//            DATA.push(sum);
-//        }
-//        else { throw new WrongTypeException("Num"); }
     }
     
-    void MOD() throws WrongTypeException,
+    public void MOD() throws WrongTypeException,
                       StackUnderflowException
     {
         calculate(new Operation() { 
             public double op(double a, double b) { return a/b; } }
         );
-        /*  */
-        /* Stackable arg1 = DATA.pop(); */
-        /* Stackable arg2 = DATA.pop(); */
-        /*  */
-        /* if(arg1.looksLikeNumber() && arg2.looksLikeNumber()) */
-        /* { */
-        /*     Num a = (Num) arg1, b = (Num) arg2; */
-        /*     Num sum = new Num(b.getNumber() % a.getNumber()); */
-        /*     DATA.push(sum); */
-        /* } */
-        /* else { throw new WrongTypeException("Num"); } */
     }
 }
