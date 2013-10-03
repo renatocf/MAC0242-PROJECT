3199ec0277a24f4b3605b11f068398aa411f5e4e
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-02 22:39:32 -0300

    Adding anonymous inner functions for refactorate tests methods

--- src/robot/function/Tests.java
+++ src/robot/function/Tests.java
@@ -29,146 +29,192 @@ public class Tests
         this.DATA = DATA;
     }
     
+    private interface Cmp
+    {
+        boolean cmp(double a, double b);
+    }
+    
     /**
-     * Assembly funcion EQ. 
+     * General-model funcion compare.
      * Makes the correspondent funcion 
      * comparison between the two elements
      * int the top of the main stack.
      */
+    void compare(Cmp cmp) throws WrongTypeException,
+                          StackUnderflowException
+    {
+        // Default answers
+        Num no = new Num(0), yes = new Num(1);
+           
+        // Stores the arguments to be tested
+        Stackable arg1 = DATA.pop(), arg2 = DATA.pop();
+        
+        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        {
+            // Downcasts to number if the type is correct
+            Num num1 = (Num) arg1, num2 = (Num) arg2;
+            
+            // Result of the comparison
+            boolean res = cmp.cmp(num1.getNumber(), num2.getNumber());
+            
+            // Push true or false accordingly to the comparison
+            /* TODO: res ? DATA.push(yes) : DATA.push(no); */
+            if(res) { DATA.push(yes); } else { DATA.push(no); }
+        }
+    }
+    
     void EQ() throws WrongTypeException,
                      StackUnderflowException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() == num2.getNumber())
-            {
-               DATA.push(yes);
-            }
-            else
-            {
-               DATA.push(no); 
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a == b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() == num2.getNumber())
+        //    {
+        //       DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //       DATA.push(no); 
+        //    }
+        //}
     }
     
     void GT() throws StackUnderflowException,
                      WrongTypeException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() < num2.getNumber())
-            {
-                DATA.push(yes);
-            }
-            else
-            {
-                DATA.push(no);
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a > b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() < num2.getNumber())
+        //    {
+        //        DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //        DATA.push(no);
+        //    }
+        //}
     }
     
     
     void GE() throws StackUnderflowException,
                      WrongTypeException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() <= num2.getNumber())
-            {
-                DATA.push(yes);
-            }
-            else
-            {
-                DATA.push(no);
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a >= b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() <= num2.getNumber())
+        //    {
+        //        DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //        DATA.push(no);
+        //    }
+        //}
     }
     
     
     void LT() throws StackUnderflowException,
                      WrongTypeException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() > num2.getNumber())
-            {
-                DATA.push(yes);
-            }
-            else
-            {
-                DATA.push(no);
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a < b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() > num2.getNumber())
+        //    {
+        //        DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //        DATA.push(no);
+        //    }
+        //}
     }
     
     
     void LE() throws StackUnderflowException,
                      WrongTypeException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() >= num2.getNumber())
-            {
-                DATA.push(yes);
-            }
-            else
-            {
-                DATA.push(no);
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a <= b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() >= num2.getNumber())
+        //    {
+        //        DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //        DATA.push(no);
+        //    }
+        //}
     }
     
     void NE() throws WrongTypeException,
                      StackUnderflowException
     {
-        Num yes = new Num(1);
-        Num no = new Num(0);
-        Stackable arg1 = DATA.pop();
-        Stackable arg2 = DATA.pop();
-        if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
-        {
-            Num num1 = (Num) arg1, num2 = (Num) arg2;
-            if(num1.getNumber() != num2.getNumber())
-            {
-               DATA.push(yes);
-            }
-            else
-            {
-               DATA.push(no); 
-            }
-        }
+        compare(new Cmp() {
+            public boolean cmp(double a, double b) { return a != b; } }
+        );
+        //Num yes = new Num(1);
+        //Num no = new Num(0);
+        //Stackable arg1 = DATA.pop();
+        //Stackable arg2 = DATA.pop();
+        //if(arg1.looksLikeNumber() && arg2.looksLikeNumber())
+        //{
+        //    Num num1 = (Num) arg1, num2 = (Num) arg2;
+        //    if(num1.getNumber() != num2.getNumber())
+        //    {
+        //       DATA.push(yes);
+        //    }
+        //    else
+        //    {
+        //       DATA.push(no); 
+        //    }
+        //}
     }
 }
