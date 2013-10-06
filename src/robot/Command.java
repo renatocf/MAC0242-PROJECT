9a1e6dbd4e20f711fb76b1d37452af888fb5d67d
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-06 16:22:42 -0300

    Changing function name from 'getCommand' to 'getFunction' (things got less messed up)

--- src/robot/Command.java
+++ src/robot/Command.java
@@ -3,23 +3,23 @@ import stackable.*;
 
 public class Command
 {
-    final private String    command;
+    final private String    function;
     final private Stackable attribute;
     final private String    label;
 
-    public Command(String cmd, Stackable atr, String lbl)
+    public Command(String func, Stackable atr, String lbl)
     {
-        this.command = cmd;
+        this.function  = func;
         this.attribute = atr;
-        this.label = lbl;
+        this.label     = lbl;
     }
 
     public String toString()
     {
-        return this.label + ": " + this.command + " " + this.attribute;
+        return this.label + ": " + this.function + " " + this.attribute;
     }
 
-    public String    getCommand   () { return this.command;   }
+    public String    getFunction  () { return this.function;  }
     public Stackable getAttribute () { return this.attribute; }
     public String    getLabel     () { return this.label;     }
 }
