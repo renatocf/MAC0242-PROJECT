fd6af03d761277e1cab466f55fb63371a5ea4f78
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-11 17:59:52 -0300

    Adding new reload.sh

--- test/dummyFindCrystal.asm
+++ test/dummyFindCrystal.asm
@@ -1,13 +1,11 @@
-    SEE
-    PUSH {crystal}
-    SEEK
-    PUSH 0
-    EQ
-    JIT mov
-    POP
-    DRAG
-mov:PUSH ->W
-    MOVE
-    END
-    
-
+     SEE
+     PUSH {crystal}
+     SEEK
+     PUSH 0
+     EQ
+     JIT mov
+     POP
+     DRAG
+mov: PUSH ->W
+     MOVE
+     END
