0faf5031a892bd20846359dfbeec1d4d1cc0e549
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-12 18:14:54 -0300

    Fixing and adding javadoc for package gui

--- src/gui/Textual.java
+++ src/gui/Textual.java
@@ -6,27 +6,54 @@ import scenario.*;
 import parameters.*;
 import stackable.item.*;
 
+/**
+ * <b>GUI - Textual Mode</b><br>
+ * Makes an implementation of the interface
+ * GUI for exhibiting the game, with colors,
+ * in a Unix shell.
+ * <p>
+ * The program may also work if the shell
+ * accepts the ANSI Escape Codes.
+ * 
+ * @author Renato Cordeiro Ferreira
+ */
 public class Textual 
     implements Game, Colors, GUI
 {
     private Map map;
 
+    /** 
+     * Default constructor.<br>
+     * @param map Object of the class map
+     *            from package arena.
+     */
     public Textual(Map map)
     {
         this.map = map;
     }
     
+    /**
+     * Paint 1 frame of the game.<br>
+     * Depending on the speedy attribute,
+     * from interface Game, exhibits a frame
+     * in the program's standart output.
+     */
     public void paint()
     {
-        try { 
-            Thread.sleep(SPEED);
-        } catch (Exception e) {
-        }
+        try { Thread.sleep(SPEED); } 
+        catch (Exception e) { }
+        
         // Clear screen
         System.out.print(CLEAR);
         printMap();
     }
     
+    /** 
+     * Shows Mini Map.<br>
+     * Creates a Mini Map with dimensions MAP_SIZE
+     * x MAP_SIZE, with a one-character representation
+     * of each scenario/item in the map.
+     */
     public void printMiniMap()
     {
         for(int i = 0; i < MAP_SIZE; i++)
@@ -39,67 +66,30 @@ public class Textual
         }
     }
     
-    final private String hexTop(int type, String midColor)
-    {
-        if(type == 1)
-            return RESTORE + "/" + midColor +  " "  + RESTORE + "\\"; /*  / \\ */
-        else
-            return RESTORE + "/" + midColor + "   " + RESTORE + "\\"; /* /   \\ */
-    }
-    
-    final private String hexBot(int type, String midColor)
-    {
-        if(type == 1)
-            return RESTORE + "\\" + midColor + "   " + RESTORE + "/"; /* \   // */
-        else
-            return RESTORE + "\\" + midColor +  " "  + RESTORE + "/"; /*  \ / */
-    }
-    
-    final private String colorof(int i, int j) //(Appearence ap)
-    {
-        Appearence ap = map.map[i][j].getAppearence();
-        
-        if(ap == null) return RESTORE;
-        switch (ap)
-        {
-            case DIRT   : return ON_BLACK; 
-            case GRASS  : return ON_IGREEN;
-            case TUNDRA : return ON_WHITE; 
-            
-            case ROCKY  : return ON_IBLACK;
-            case ICE    : return ON_CYAN;  
-            case JUNGLE : return ON_IGREEN;
-            case WATER  : return ON_BLUE;  
-            case SAND   : return ON_YELLOW;
-            default     : return RESTORE;  
-        }
-    }
-    
-    final private Scenario scen(int i, int j)
-    {
-        if(i < 0 || j < 0 || i >= MAP_SIZE || j >= MAP_SIZE) return null;
-        return map.map[i][j].getScenario();
-    }
-    
-    final private Item item(int i, int j)
-    {
-        if(i < 0 || j < 0 || i >= MAP_SIZE || j >= MAP_SIZE) return null;
-        return map.map[i][j].getItem();
-    }
-
-    final private void print(String s)
-    {
-        System.out.print(s);
-    }
-    
-    final private void println(String s)
-    {
-        System.out.println(s);
-    }
-    
+    /**
+     * Shows Map.<br>
+     * Creates a Map with more details of each element
+     * of the scenarios and items in the map. Each 
+     * hexagon represents one title. 
+     */
+    /*
+     *  .--------------------------------------------.
+     *  |                   INDEX                    |
+     *  |---------------.----------------------------.
+     *  | Items:        | Scenarios:                 |
+     *  |===============|============================|
+     *  |               |                            |
+     *  | - Crystal: /\ | - Base:  ßß   - Water: ≈≈  |
+     *  |            \/ |          ßß            ≈≈  |
+     *  | - Stone:   .. | - Rock:  ⌈⌉   - Robot: ()  |
+     *  |            ¨¨ |          ⌊⌋            /\  |
+     *  |               | - Trees: ☘☘                |
+     *  |               |          ☘☘                |
+     *  '---------------'----------------------------'
+     */
     public void printMap()
     {
-        // Print scenario top
+        // Print map top
         for(int j = 0; j < MAP_SIZE-1; j++)
             print("   .  ");
         println("   .");
@@ -117,10 +107,10 @@ public class Textual
         // Print ordinary lines
         for(int i = 0; i < MAP_SIZE; i++)
         {
-            /* print(ON_GREEN); */
+            // Checks it the line is odd or even.
             boolean odd = (i % 2 == 1) ? true : false;
             
-            // Print itens and scenario
+            // Center top of the hexagons
             print( (odd) ? "   " : "");
             for(int j = 0; j < MAP_SIZE; j++)
             {
@@ -148,7 +138,7 @@ public class Textual
             }
             println(RESTORE + "|");
             
-            // Center of hexagons
+            // Center bottom of the hexagons
             print( (odd) ? "   " : "");
             for(int j = 0; j < MAP_SIZE; j++)
             {
@@ -178,7 +168,7 @@ public class Textual
             
             if(i == MAP_SIZE-1) break;
 
-            // First line of hexagons
+            // First line of hexagons top/bottom
             print( (odd) 
                     ? "  "  + hexTop(1, colorof(i,  0) ) 
                     : " "   + hexBot(1, colorof(i+1,0) )
@@ -193,7 +183,7 @@ public class Textual
                     : colorof(i+1,MAP_SIZE-1) + " "   + RESTORE + "\\"
             );
             
-            // Second line of hexagons
+            // Second line of hexagons top/bottom
             print( (odd) 
                     ? " "   + hexTop(2, colorof(i,0))
                     : "  "  + hexBot(2, colorof(i,0))
@@ -209,7 +199,7 @@ public class Textual
             ); 
         }
             
-        // Print scenario bottom
+        // Print map bottom
         print(" " + hexBot(1, colorof(MAP_SIZE-1,0)) );
         for(int j = 1; j < MAP_SIZE; j++)
             print(" " + hexBot(1, colorof(MAP_SIZE-1,j)) );
@@ -223,5 +213,123 @@ public class Textual
         for(int j = 0; j < MAP_SIZE-1; j++)
             print("   '  ");
         println("   '");
+    }
+    
+    /**
+     * Auxiliar function for printing: hexagon top.<br>
+     * Encapsulates the printing of an hexagon top.
+     * 
+     * @param  type     If 1, is the top most part of the 
+     *                  hexagon. Otherwise, is the base of it
+     * @param  midColor Color which may be between the 
+     *                  lines of the form.
+     * @return String with one of the parts of the hexagon top.
+     */
+    final private String hexTop(int type, String midColor)
+    {
+        if(type == 1)
+            return RESTORE + "/" + midColor +  " "  + RESTORE + "\\"; /*  / \\ */
+        else
+            return RESTORE + "/" + midColor + "   " + RESTORE + "\\"; /* /   \\ */
+    }
+    
+    /**
+     * Auxiliar function for printing: hexagon bottom.<br>
+     * Encapsulates the printing of an hexagon top.
+     * 
+     * @param  type     If 1, is the upper part of the bottom of the 
+     *                  hexagon. Otherwise, is the end of it.
+     * @param  midColor Color which may be between the 
+     *                  lines of the form.
+     * @return String with one of the parts of the hexagon bottom.
+     */
+    final private String hexBot(int type, String midColor)
+    {
+        if(type == 1)
+            return RESTORE + "\\" + midColor + "   " + RESTORE + "/"; /* \   // */
+        else
+            return RESTORE + "\\" + midColor +  " "  + RESTORE + "/"; /*  \ / */
+    }
+    
+    /**
+     * Auxiliar getter for: terrain's appearence color.<br>
+     * Encapsulates the printing of an hexagon top.
+     * 
+     * @param  i Line position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @param  j Colum position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @return String with the color which matches the 
+     *         appearence for the terrain in the (i,j) 
+     *         position 
+     */
+    final private String colorof(int i, int j) //(Appearence ap)
+    {
+        Appearence ap = map.map[i][j].getAppearence();
+        
+        if(ap == null) return RESTORE;
+        switch (ap)
+        {
+            case DIRT   : return ON_BLACK; 
+            case GRASS  : return ON_IGREEN;
+            case TUNDRA : return ON_WHITE; 
+            
+            case ROCKY  : return ON_IBLACK;
+            case ICE    : return ON_CYAN;  
+            case JUNGLE : return ON_IGREEN;
+            case WATER  : return ON_BLUE;  
+            case SAND   : return ON_YELLOW;
+            default     : return RESTORE;  
+        }
+    }
+    
+    /**
+     * Auxiliar getter for: terrain's scenario.<br>
+     * Encapsulates the printing of an hexagon top.
+     * 
+     * @param  i Line position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @param  j Colum position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @return Scenario for the terrain in the (i,j) position 
+     */
+    final private Scenario scen(int i, int j)
+    {
+        if(i < 0 || j < 0 || i >= MAP_SIZE || j >= MAP_SIZE) return null;
+        return map.map[i][j].getScenario();
+    }
+    
+    /**
+     * Auxiliar getter for: terrain's item.<br>
+     * Encapsulates the printing of an hexagon top.
+     * 
+     * @param  i Line position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @param  j Colum position in the terrain matrix
+     *           (from 0 to MAP_SIZE-1)
+     * @return Item for the terrain in the (i,j) position 
+     */
+    final private Item item(int i, int j)
+    {
+        if(i < 0 || j < 0 || i >= MAP_SIZE || j >= MAP_SIZE) return null;
+        return map.map[i][j].getItem();
+    }
+
+    /**
+     * Encapsulate printing function (without \n).
+     * @param s String to be printed.
+     */
+    final private void print(String s)
+    {
+        System.out.print(s);
+    }
+    
+    /**
+     * Encapsulate printing function (with \n).
+     * @param s String to be printed.
+     */
+    final private void println(String s)
+    {
+        System.out.println(s);
     }
 }
---
+++ src/gui/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <b>Defines an interface for the visual
+ * exhibition of the game (GUI).</b>
+ * <p>
+ */
+package gui;
