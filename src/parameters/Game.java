c3d8e4f529ccac58ee531e61a8491ab686730a84
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-13 01:28:00 -0300

    Changing MAP_SIZE back to 15x15

--- src/parameters/Game.java
+++ src/parameters/Game.java
@@ -2,7 +2,7 @@ package parameters;
 
 public interface Game
 {
-    public static int MAP_SIZE           = 50;
+    public static int MAP_SIZE           = 15;
     public static int ROBOTS_NUM_MAX     = 100;
     public static int ROBOTS_NUM_INITIAL = 3;
     public static int SPEED = 400;
