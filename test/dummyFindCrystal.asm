6e80538eb620af0a685ce4a218a82aaca428ca15
Karina Awoki <k.sueminha@hotmail.com>
2013-10-17 16:52:30 -0300

    Adding simple (aka dummy) algorithm to find a crystal

---
+++ test/dummyFindCrystal.asm
@@ -0,0 +1,20 @@
+    SEE
+    PUSH {crystal}
+    SEEK
+    PUSH 0
+    EQ
+    JIT mov
+    POP
+    DRAG
+    PUSH 1
+    EQ
+    JIT mov
+    POP
+    POP
+    POP
+    DRAG
+mov:PUSH ->W
+    MOVE
+    END
+    
+
