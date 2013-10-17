91c0265c4561486ccb0d4cdb15c825014bb130a6
Karina Awoki <k.sueminha@hotmail.com>
2013-10-17 17:04:40 -0300

    Changing test for testing

--- test/test.asm
+++ test/test.asm
@@ -1,3 +1,19 @@
-PUSH ->W
-MOVE
-END
+        PUSH ->W
+        MOVE
+        POP
+        PUSH ->W
+        MOVE
+        POP
+while:  SEE
+        PUSH {crystal}
+        SEEK
+        JIF fim
+        PUSH 2
+        EQ 
+        JIT fim
+        DRAG
+        POP
+        PUSH ->W
+        DROP
+        POP
+fim:    JMP while
