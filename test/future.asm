20d80c9ccf9dbc3ddce9095d5b9ecb2953d77971
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-18 12:28:43 -0300

    Alignments

--- test/future.asm
+++ test/future.asm
@@ -1,18 +1,18 @@
-     PUSH ->W
-     DUP
-     LOOK
-     ITEM
-     PUSH {crystal}
-     EQ
-     JIF  end
-     DRAG
-     PUSH 1
-     JIT moveup
-end: PUSH ->W
-     MOVE
-     END
-moveup: PUSH ->NE
-        MOVE
-        PUSH ->NW
-        MOVE
-        JMP moveup
+        PUSH    ->W
+        DUP     
+        LOOK    
+        ITEM    
+        PUSH    {crystal}
+        EQ
+        JIF     end
+        DRAG    
+        PUSH    1
+        JIT     moveup
+end:    PUSH    ->W
+        MOVE    
+        END
+moveup: PUSH    ->NE
+        MOVE    
+        PUSH    ->NW
+        MOVE    
+        JMP     moveup
