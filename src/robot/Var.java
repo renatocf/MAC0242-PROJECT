c47ec4791126e95c57d34e84915d20be7cdd67be
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-27 08:45:31 -0200

    Creating new class and exception for local variables

---
+++ src/robot/Var.java
@@ -0,0 +1,146 @@
+package robot;
+
+// Default libraries
+import java.util.Vector;
+
+// Libraries
+import stackable.*;
+import exception.*;
+
+/**
+ * <b>Assembly functions - class Var</b><br>
+ * Provides the funcions for allocating,
+ * freeing, setting and getting local 
+ * variables.
+ * 
+ * @author Renato Cordeiro Ferreira
+ * @see Ctrl
+ */
+final public class Var
+{
+    // No instances of this class allowed
+    private Var() {} 
+    
+    /**
+     * Assembly funcion ALOC. <br>
+     * Allocates a variable in the local
+     * stack. If it is already defined, 
+     * throws an exception to warn about
+     * the wrong memory usage.
+     *
+     * @param  rvm  Virtual Machine
+     * @param  name Variable name 
+     *              (to be defined)
+     *                  
+     * @throws SegmentationFaultException
+     * @throws WrongTypeException
+     */
+    static void ALOC(RVM rvm, Stackable name)
+        throws SegmentationFaultException,
+               WrongTypeException
+    {
+        if(name instanceof Text)
+        {
+            Text t = (Text) name;
+            String n = t.getText();
+            if(!rvm.VARS.containsKey(n))
+                rvm.VARS.put(n, null);
+            else throw new SegmentationFaultException();
+        }
+        else throw new WrongTypeException("a name to a variable");
+    }
+    
+    /**
+     * Assembly funcion FREE. <br>
+     * Frees a variable, returning its    
+     * memory to the virtual machine. If   
+     * the name is not defined in the 
+     * stack, throws an exception to warn
+     * about the wrong memory usage.
+     *
+     * @param  rvm  Virtual Machine
+     * @param  name Variable name 
+     *              (to be freed)
+     *                  
+     * @throws SegmentationFaultException
+     * @throws WrongTypeException
+     */
+    static void FREE(RVM rvm, Stackable name) 
+        throws SegmentationFaultException,
+               WrongTypeException
+    {
+        if(name instanceof Text)
+        {
+            Text t = (Text) name;
+            String n = t.getText();
+            if(rvm.VARS.containsKey(n))
+                rvm.VARS.remove(n);
+            else throw new SegmentationFaultException();
+        }
+        else throw new WrongTypeException("a name to a variable");
+    }
+    
+    /**
+     * Assembly funcion SET. <br>
+     * Sets a local variable with the value
+     * in the top of the main stack. If the
+     * variable was not allocated, throws 
+     * an exception to warn about the wrong 
+     * memory usage.
+     *
+     * @param  rvm  Virtual Machine
+     * @param  name Variable name 
+     *              (to be setted)
+     *                  
+     * @throws SegmentationFaultException
+     * @throws WrongTypeException
+     */
+    static void SET(RVM rvm, Stackable name)
+        throws SegmentationFaultException,
+               WrongTypeException
+    {
+        if(name instanceof Text)
+        {
+            Text t = (Text) name;
+            String n = t.getText();
+            if(rvm.VARS.containsKey(n))
+                rvm.VARS.put(n, rvm.DATA.pop());
+            else throw new SegmentationFaultException();
+        }
+        else throw new WrongTypeException("a name to a variable");
+    }
+    
+    /**
+     * Assembly funcion GET. <br>
+     * Gets the value stored in the 
+     * memory (if avaiable) and puts it in 
+     * the top of the main stack.
+     *
+     * @param  rvm  Virtual Machine
+     * @param  name Variable name (to have 
+     *              its value returned)
+     *                  
+     * @throws NotInitializedException
+     * @throws SegmentationFaultException
+     * @throws WrongTypeException
+     */
+    static void GET(RVM rvm, Stackable name) 
+        throws NotInitializedException,
+               SegmentationFaultException,
+               WrongTypeException
+    {
+        if(name instanceof Text)
+        {
+            Text t = (Text) name;
+            String n = t.getText();
+            if(rvm.VARS.containsKey(n))
+            {
+                Stackable top = rvm.VARS.get(n);
+                if(top != null) rvm.DATA.push(top);
+                else throw new NotInitializedException(n);
+            }
+            else throw new SegmentationFaultException();
+        }
+        else throw new WrongTypeException("a name to a variable");
+    }
+}
