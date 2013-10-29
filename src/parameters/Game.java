bb471482fa114da1890d03f7cdfbf6ef7deebe86
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-29 10:54:36 -0200

    Adding javadoc for Game

--- src/parameters/Game.java
+++ src/parameters/Game.java
@@ -1,17 +1,66 @@
 package parameters;
 
+/** 
+ * <b>Game</b><br>
+ * General interface with parameters used to
+ * configure the general behaviors of the game.
+ */
 public interface Game
 {
-    // Animation
-    public static int SPEED              = 300;
+    // /////////////////////////////////////////////////////////////////
+    // ----------------------------------------------------------------
+    //                            ANIMATION                              
+    // ----------------------------------------------------------------
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
-    // Map
-    public static int MAP_SIZE           = 15;
+    /** 
+     * <b>Animation: speed</b><br>
+     * Number of miliseconds before painting
+     * a new screen.
+     */
+    public static int SPEED = 300;
     
-    // Robots
-    public static int ROBOTS_NUM_MAX     = 100;
+    // /////////////////////////////////////////////////////////////////
+    // ----------------------------------------------------------------
+    //                               MAP                                 
+    // ----------------------------------------------------------------
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+    /**
+     * <b>Map: size</b><br>
+     * Dimensions for the map. In a common
+     * screen, 15x15 is a good size.
+     */
+    public static int MAP_SIZE = 15;
+    
+    // /////////////////////////////////////////////////////////////////
+    // ----------------------------------------------------------------
+    //                             ROBOTS                               
+    // ----------------------------------------------------------------
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+    /** <b>Robots: max number</b><br>
+     * Maximum number of robots per player
+     * accepted by the arena 
+     */
+    public static int ROBOTS_NUM_MAX = 100;
+    /**
+     * <b>Robots: initial number</b><br>
+     * Initial number od robots per player
+     * created when the game starts.
+     */
     public static int ROBOTS_NUM_INITIAL = 3;
     
-    // Assembly
-    public static int ASM_MAX_RUN        = 300;
+    // /////////////////////////////////////////////////////////////////
+    // ----------------------------------------------------------------
+    //                            ASSEMBLY                               
+    // ----------------------------------------------------------------
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * <b>Assembly: maximum run</b><br>
+     * Maximum of assembly lines to be run 
+     * by a program that makes no syscall.
+     */
+    public static int ASM_MAX_RUN = 300;
 }
