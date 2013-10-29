4f2918284f0ed2bac037137cdb411fee491a1999
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-10-29 16:39:00 -0200

    Adding javadoc to package stackable.item

---
+++ src/stackable/item/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <b>
+ * Defines valid types both inside the main 
+ * stack of a robot virtual machine (RVM) -
+ * as a stackable - and also inside a robot
+ * cargo - as a catchable item in the map.
+ * </b>
+ * @see arena.Robot
+ * @see stackable.Stackable
+ */
+package stackable.item;
