e55a649c130c6c0ab6c9cf0f6f76bd34d1e0d360
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-03 17:40:24 -0200

    Adding a new class for holding players

---
+++ src/players/Player.java
@@ -0,0 +1,60 @@
+package players;
+
+// Default libraries
+import java.util.ArrayList;
+
+// Libraries
+import gui.Colors;
+import arena.Robot;
+import scenario.Base;
+
+// Static symbols
+import static gui.Colors.*;
+
+public class Player
+{
+    static int total = 0;
+    
+    private int    ID;
+    private Base   base;
+    private String color;
+    private int[]  basePos;
+    
+    private // Player's armies
+    ArrayList<Robot> armies = new ArrayList<Robot>();
+    
+    public Player(Base base)
+    {
+        this.ID    = ++total;
+        this.base  = base;
+        this.color = setColor(this.ID);
+        
+        // Sets the base as his
+        base.own(this);
+    }
+    
+    public int    getID() { return this.ID;    }
+    public String color() { return this.color; }
+    
+    public void addArmy(Robot newRobot)
+    {
+        this.armies.add(newRobot);
+    }
+    
+    public void removeArmy(Robot deadRobot)
+    {
+        this.armies.remove(deadRobot);
+    }
+    
+    final private String setColor(int plID)
+    {
+        switch(plID)
+        {
+            case 1:  return ON_BLACK;
+            case 2:  return ON_RED;
+            case 3:  return ON_GREEN;
+            case 4:  return ON_YELLOW;
+            default: return "";
+        }
+    }
+}
