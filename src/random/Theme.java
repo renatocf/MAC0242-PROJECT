cb62a8b0c44cf2adf08a67bfa74c72b6025e1a69
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-03 14:12:56 -0200

    Addint 'Thematic' interface

--- src/random/Theme.java
+++ src/random/Theme.java
@@ -6,7 +6,20 @@ package random;
  *
  * @author Vinicius Silva
  */
-interface Theme
+public interface Theme
 {
-    char [][] generateMatrix(int side);
+    char[][] generateMatrix (int side);
+}
+
+/**
+ * <b>Thematic</b><br>
+ * Simple interface extend Theme, demmanding
+ * from the Themes having a way of putting 
+ * bases when creating a map.
+ *
+ * @author Renato Cordeiro Ferreira
+ */
+interface Thematic extends Theme
+{
+    char[][] putBases (char[][] map);
 }
