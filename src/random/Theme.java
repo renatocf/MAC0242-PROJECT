77513f4786d9a745eee5a6a6aa790ed7599af8ec
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-04 15:25:49 -0200

    generateMatrix not public any more

--- src/random/Theme.java
+++ src/random/Theme.java
@@ -9,10 +9,8 @@ package random;
  */
 abstract class Theme
 {
-    // Abstract methods
-    abstract public char[][] generateMatrix (int side);
-    
     // Package leven (only)
+    abstract char[][] generateMatrix (int side);
     abstract char[][] putBases    (char[][] map);
     abstract char[][] putCrystals (char[][] map);
     abstract char[][] putRocks    (char[][] map);
