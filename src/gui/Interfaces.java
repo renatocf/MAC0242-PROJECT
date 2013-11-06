1efebd82acc8afdda0109664be2c16bdb5525063
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-06 07:25:14 -0200

    Adding new interface for different types of GUI

---
+++ src/gui/Interfaces.java
@@ -0,0 +1,17 @@
+package gui;
+
+/**
+ * <b>Interfaces</b><br>
+ * Simple interface for all avaiable types
+ * of interfaces implemented inside package
+ * GUI (useful for switching the implementations).
+ * 
+ * @see GUI
+ * @see main.Main
+ * @author Renato Cordeiro Ferreira
+ */
+public enum Interfaces
+{
+    TEXTUAL,
+    GRAPHICAL
+}
