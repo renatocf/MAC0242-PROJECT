4d453492e83f76fa02519f2a0167c89fe3cdf8c0
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-06 21:32:02 -0200

    Adding new interface for defining a printable element: something that has an associated image

---
+++ src/gui/Printable.java
@@ -0,0 +1,17 @@
+package gui;
+
+/**
+ * Interface for requiring for a 
+ * class/enum providing a 'name' 
+ * class, which must return the 
+ * own class name.
+ */
+public interface Printable
+{
+    /**
+     * Method required by interface
+     * Printable.
+     * @return Class name
+     */
+    public String name();
+}
