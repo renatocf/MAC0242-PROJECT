d92c296b5d02b62f6af747acec06f63168c400f6
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-08 11:44:15 -0200

    Adding package documentation for gui.graphical and gui.textual classes

---
+++ src/gui/graphical/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <b>
+ * Defines a Graphical User Interface 
+ * for the game (using AWT and Swing).
+ * </b>
+ * @see gui
+ */
+package gui.graphical;
---
+++ src/gui/textual/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <b>
+ * Defines a Textual User Interface for the 
+ * game (using <em>Ansi Escape Colors</em>
+ * for terminals).
+ * </b>
+ * @see gui
+ */
+package gui.textual;
