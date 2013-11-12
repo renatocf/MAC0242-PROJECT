e5605cc8b283d5f0211950c59013e8dcab4c6648
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2013-11-12 14:21:30 -0200

    Discriminated the return errors

---
+++ src/robot/Returns.java
@@ -0,0 +1,19 @@
+package robot;
+
+public interface Returns
+{   
+    public final static int TRUE            =  1;
+    public final static int SUCCEDED        =  1;
+
+    public final static int FALSE           =  0;
+    public final static int NOT_SUCCEDED    =  0;
+    public final static int NO_TARGET       =  0;
+    public final static int NO_ENERGY       =  0; //= -1;
+    public final static int END_OF_MAP      =  0; //= -2;
+    public final static int BLOCKED         =  0; //= -2;
+    public final static int FULL_SLOTS      =  0; //= -3;
+    public final static int EMPTY_SLOTS     =  0; //= -3;
+    public final static int OUT_OF_RANGE    =  0; //= -3;
+    public final static int FRIENDLY_FIRE   =  0; //= -4;
+    public final static int INVALID_ACTION  =  0; //= -5;
+}
