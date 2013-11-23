f5f0579a30149e22801ddf6ccdc2d6e136ea5d0e
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-23 09:27:17 -0200

    Adding missing class parameters (still need to recover the old completed one)

---
+++ src/parser/Parameters.java
@@ -0,0 +1,16 @@
+package parser;
+
+class Parameters
+{
+    private String[] types;
+    
+    /**
+     * Default constructor.<br>
+     * @param types Array with strings representing
+     *              the types of the arrays
+     */
+    Parameters(String[] types)
+    {
+        this.types = types;
+    }
+}
