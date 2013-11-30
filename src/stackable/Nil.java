812d5d2701c303cdb1e49fb494b047a3b61c10ac
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-11-30 12:56:02 -0200

    Creating a new stackable to mean 'no information'

---
+++ src/stackable/Nil.java
@@ -0,0 +1,48 @@
+/**********************************************************************/
+/* Copyright 2013 KRV                                                 */
+/*                                                                    */
+/* Licensed under the Apache License, Version 2.0 (the "License");    */
+/* you may not use this file except in compliance with the License.   */
+/* You may obtain a copy of the License at                            */
+/*                                                                    */
+/*  http://www.apache.org/licenses/LICENSE-2.0                        */
+/*                                                                    */
+/* Unless required by applicable law or agreed to in writing,         */
+/* software distributed under the License is distributed on an        */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       */
+/* either express or implied.                                         */
+/* See the License for the specific language governing permissions    */
+/* and limitations under the License.                                 */
+/**********************************************************************/
+package stackable;
+
+/**
+ * <b>Stackable - Nil</b><br>
+ * Stackable to show that this is
+ * a meaningless information.
+ * 
+ * @author Renato Cordeiro Ferreira
+ */
+public class Nil implements Stackable
+{
+    // Singleton object for nil
+    private static Nil nil = new Nil();
+    
+    // No instances of this class allowed
+    private Nil() {};
+    
+    /**
+     * Default getter.<br>
+     * @return Singleton object nil
+     */
+    public static Nil get()
+    {
+        return Nil.nil;
+    }
+    
+    // Interface stackable
+    public String toString()
+    {
+        return "Nil";
+    }
+}
