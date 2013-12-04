5ae9bbef9a23dc3d7b03f85c65c6a1277732803b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2013-12-04 07:57:04 -0200

    Adding NOTICE and making the project compile

--- src/gui/graphical/Menu.java
+++ src/gui/graphical/Menu.java
@@ -1,15 +1,31 @@
+/**********************************************************************/
+/* Copyright 2013 KRV                                                 */
+/*                                                                    */
+/* Licensed under the Apache License, Version 2.0 (the "License");    */
+/* you may not use this file except in compliance with the License.   */
+/* You may obtain a copy of the License at                            */
+/*                                                                    */
+/*  http://www.apache.org/licenses/LICENSE-2.0                        */
+/*                                                                    */
+/* Unless required by applicable law or agreed to in writing,         */
+/* software distributed under the License is distributed on an        */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       */
+/* either express or implied.                                         */
+/* See the License for the specific language governing permissions    */
+/* and limitations under the License.                                 */
+/**********************************************************************/
+package gui.graphical;
+
+// Graphical Libraries (AWT)
+import java.awt.Graphics;
+
+// Graphical Libraries (Swing)
+import javax.swing.JPanel;
+
 class Menu extends JPanel 
 {
-
-
     protected void paintComponent(Graphics g) 
     {
         super.paintComponent(g);
-                        
-        
     }
 }
-
-
-
-
