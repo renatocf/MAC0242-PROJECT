2c22aa35926aa17c4437d6ee9a07c0c8c10a5731
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2013-12-05 16:57:02 -0200

    Now around knows the coordinates

--- src/stackable/Around.java
+++ src/stackable/Around.java
@@ -35,6 +35,7 @@ import stackable.item.*;
 public class Around implements Stackable
 {
     public String [][] matrix;
+           Coordinate[] coords;
     
     /** 
      * Default constructor.
@@ -42,9 +43,10 @@ public class Around implements Stackable
      *               that is seeing its 
      *               neighborhood.
      */
-    public Around(Terrain[] seeing)
+    public Around(Terrain[] seeing, Coordinate[] coords)
     {
-        matrix = new String[2][seeing.length];
+        matrix = new String[2] [seeing.length];
+        this.coords = coords;
         for(int i = 0; i< seeing.length; i++)
         {
             if (seeing[i] == null) matrix[0][i] = "";
@@ -52,76 +54,29 @@ public class Around implements Stackable
             {
                 Scenario s = seeing[i].getScenario();
                 if(s == null) matrix[0][i] = " ";
-                else          matrix[0][i] = s.getClass().getName();
-
-                //Adjusting the String for simplicity
-                if(matrix[0][i].equals("players.Base"))
+                else
                 {
-                    matrix[0][i] = "Base";
+                    matrix[0][i] = s.getClass().getName().replaceFirst("^.*\\.", "");
                 }
-                
+                          
                 //Adjusting the String for simplicity
-                if(matrix[0][i].equals("arena.Robot") && i != 0)
+                if(matrix[0][i].equals("Robot") && i != 0)
                 {
                     Robot r = (Robot) s;
-                    matrix[0][i] = "Robot" + r.getTeam().getID();
+                    matrix[0][i] += r.getTeam().getID();
                 }
                 
                 stackable.item.Item it = seeing[i].getItem(); 
                 if(it == null) matrix[1][i] = " ";
-                else          matrix[1][i] = it.getClass().getName();
+                else           matrix[1][i] = it.getClass().getName().replaceFirst("^.*\\.", "");
             }
         }
     }
     
+    public Coordinate getCoordinate(int i)
+    {
+        return coords[i];
+    }
+    
     public String toString() { return "( Around )"; }
 }
-    
-// Some methods in case they are needed
-// in the future.
-
-//public String[] indexToPosition(int index)
-//{
-//    String ret[] = new String[2];
-//    switch (index)
-//    {
-//        case   0: break;   
-//        case   1: ret[0] = "->E" ; break;
-//        case   2: ret[0] = "->NE"; break;
-//        case   3: ret[0] = "->NW"; break;
-//        case   4: ret[0] = "->W" ; break;
-//        case   5: ret[0] = "->SW"; break;
-//        case   6: ret[0] = "->SE"; break;
-//        case   7: ret[0] = "->E" ; ret[1] = "->E" ; break;
-//        case   8: ret[0] = "->E" ; ret[1] = "->NE"; break;
-//        case   9: ret[0] = "->NE"; ret[1] = "->NE"; break;
-//        case  10: ret[0] = "->NE"; ret[1] = "->NW"; break;
-//        case  11: ret[0] = "->NW"; ret[1] = "->NW"; break;
-//        case  12: ret[0] = "->NW"; ret[1] = "->W" ; break;
-//        case  13: ret[0] = "->W" ; ret[1] = "->W" ; break;
-//        case  14: ret[0] = "->W" ; ret[1] = "->SW"; break;
-//        case  15: ret[0] = "->SW"; ret[1] = "->SW"; break;
-//        case  16: ret[0] = "->SW"; ret[1] = "->SE"; break; 
-//        case  17: ret[0] = "->SE"; ret[1] = "->SE"; break;
-//        case  18: ret[0] = "->SE"; ret[1] = "->E" ; break;                          
-//        default : ret = null;
-//    }
-//    return ret;
-//}
-
-//public void print()
-//{
-//    String pre = "    [AROUND] ";
-//    System.out.print(pre + "Scenario: ");
-//    for(String s: matrix[0])
-//    {
-//        System.out.print(s + " " );
-//    }   
-//    
-//    System.out.print("\n" + pre + "Item: ");
-//    for(String s: matrix[1])
-//    {
-//        System.out.print(s + " " );
-//    } 
-//    System.out.println();
-//}
